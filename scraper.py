7e06156e3217e5ddeb6305d9bcefc5a6d5317fea
MathiasNordZ <mathias.erik.nord@gmail.com>
2024-06-06 00:26:31 +0200

    Small changes

--- scraper.py
+++ scraper.py
@@ -4,51 +4,58 @@ import time
 import csv
 
 base_url = 'https://www.finn.no/car/used/search.html?model=1.749.7967&page='
-pages = 5
+pages = 1
 
 price = []
 make = []
 year = []
 mileage = []
 
-for page in range(1, pages+1):
-    url = f'{base_url}{pages}'
+for page in range(1, pages + 1):
+    url = f'{base_url}{page}'
     
     r = requests.get(url)
     
     if r.status_code == 200:
         soup = BeautifulSoup(r.content, 'html5lib')
         
-        make_element = soup.find_all(class_='sf-search-ad-link link link--dark hover:no-underline')
-        info_element = soup.find_all(class_ = 'mb-8 flex justify-between whitespace-nowrap font-bold')
+        make_elements = soup.find_all(class_='sf-search-ad-link link link--dark hover:no-underline')
+        info_elements = soup.find_all(class_ = 'mb-8 flex justify-between whitespace-nowrap font-bold')
+
+        for element in make_elements:
+            link_text = (element.get_text(strip=True))
+            make.append(link_text)
         
-        for info in info_element:
+        for info in info_elements:
             span_element = info.find_all('span')
+
             if len(span_element) > 0:
-                first_span = span_element[0]
-                year_span = (first_span.get_text(strip=True))
+                year_span = (span_element[0].get_text(strip=True))
                 year.append(year_span)
+            else:
+                year.append('')
                 
             if len(span_element) > 1:
-                second_span = span_element[1]
-                mileage_span = (second_span.get_text(strip=True))
+                mileage_span = (span_element[1].get_text(strip=True))
                 mileage.append(mileage_span)
+            else:
+                year.append('')
                 
             if len(span_element) > 2:
-                third_span = span_element[2]
-                price_span = (third_span.get_text(strip=True))
+                price_span = (span_element[2].get_text(strip=True))
                 price.append(price_span)
-                
-        for make1 in make_element:
-            make_element = info.find_all('span')
-            if len(make_element) > 0:
-                first_span = make_element[0]
-                make_span = (first_span.get_text(strip=True))
-                make.append(make_span)
+            else:
+                year.append('')
         
         time.sleep(1)
     else:
         print('Failed to retrieve page')
+
+price_unit = 'kr'
+mileage_unit = 'km'
+
+price = [sub.replace(price_unit,'').strip() for sub in price]
+mileage = [sub.replace(mileage_unit,'').strip() for sub in mileage]
         
 with open('carinfo.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
@@ -56,28 +63,11 @@ with open('carinfo.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer.writerow(['Navn', 'Årsmodell', 'Kilometer', 'Pris'])
     
     for i in range(max(len(make), len(year), len(mileage), len(price))):
-        row = []
-        
-        if i < len(make):
-            row.append(make[i])
-        else:
-            row.append('')
-            
-        if i < len(year):
-            row.append(year[i])
-        else:
-            row.append('')
-            
-        if i < len(mileage):
-            row.append(mileage[i])
-        else:
-            row.append('')
-            
-        if i < len(price):
-            row.append(price[i])
-        else:
-            row.append('')
+        row = [
+            make[i] if i < len(make) else '',
+            year[i] if i < len(year) else '',
+            mileage[i] if i < len(mileage) else '',
+            price[i] if i < len(price) else '',
+        ]
             
         writer.writerow(row)
-
-print(make)
